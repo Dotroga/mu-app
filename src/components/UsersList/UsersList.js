9515c751bf969b1650903cdd6b917d8e03de2295
Vasili Karpenkin <v.karpenkin@gmail.com>
2022-09-17 12:44:54 +0300

    extends React.Component

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -3,28 +3,33 @@ import s from './UsersList.modules.css'
 import * as axios from "axios";
 import userPhoto from './../../img/user.png'
 
-let UsersList = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+class UsersList extends React.Component {
+    constructor(props) {
+        super(props)
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
     }
-    return <div className={s.container}>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return <div className={s.container}>
+            {
+                this.props.users.map(u => <div key={u.id}>
                     <img  src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
-                <div>
-                    {u.followed
-                        ? <button  onClick={() => {props.follow(u.id)}} >Follow</button>
-                        : <button  onClick={() => {props.unfollow(u.id)}}>Unfollow</button>}
-                </div>
-                <div>{u.name} {u.surname}</div>
-                <div>{u.status}</div>
-                <div>{u.age} лет</div>
-                <div></div>
-            </div>)
-        }
-    </div>
+                    <div>
+                        {u.followed
+                            ? <button  onClick={() => {this.props.follow(u.id)}} >Follow</button>
+                            : <button  onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>}
+                    </div>
+                    <div>{u.name} {u.surname}</div>
+                    <div>{u.status}</div>
+                    <div>{u.age} лет</div>
+                    <div></div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 
 export default UsersList
